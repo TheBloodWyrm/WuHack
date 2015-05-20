0562b051dfd613b3932e022224113655ff469854
petpac12 <petpac12@new.edvhtl.at>
2015-05-20 09:49:05 +0200

    irgentwas

--- WuHack/src/wuhack/FXMLGUIController.java
+++ WuHack/src/wuhack/FXMLGUIController.java
@@ -5,14 +5,23 @@
  */
 package wuhack;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 /**
@@ -30,7 +39,28 @@ public class FXMLGUIController implements Initializable
   private WebView wv;
   
   private Kürzel kürzel;
+  public URL url;
   
+  public void onCheckBox() throws Exception{
+    WebEngine webEngine = wv.getEngine();
+    for(int i = 1; i < 32; i++){
+    url = new URL("https://supplierplan.htl-kaindorf.at/supp_neu/20/c/c0000" + i + ".htm");
+    ScheduleModel m = new ScheduleModel();
+    m.analyzeDoc(webEngine.documentProperty().get());
+    System.out.println(url);
+    }
+    
+    BufferedReader in = new BufferedReader(
+    new InputStreamReader(url.openStream()));
+
+    String inputLine;
+    while ((inputLine = in.readLine()) != null)
+      System.out.println(inputLine);
+    in.close();
+    
+    
+    webEngine.load(url);
+  }
   
   
   @Override
@@ -38,7 +68,21 @@ public class FXMLGUIController implements Initializable
   {
     cbLehrer.getItems().addAll(kürzel.values());
     cbLehrer.setPromptText("Lehrer Kürzel");   
-    
+    cbLehrer.setOnAction(new EventHandler()
+    {
+
+      @Override
+      public void handle(Event event)
+      {
+        try
+        {
+          onCheckBox();
+        } catch (Exception ex)
+        {
+          System.out.println(ex.getMessage());
+        }
+      }
+    });
   }
   
 }
