96a674ef0dc9a8273b0b21152ae7f8ca48da32f6
reijuc12 <reijuc12@htl-kaindorf.at>
2015-06-26 12:57:19 +0200

    stuff :neutral_face:

--- WuHack/src/wuhack/ScheduleModel.java
+++ WuHack/src/wuhack/ScheduleModel.java
@@ -25,362 +25,292 @@ import org.w3c.dom.html.HTMLTableRowElement;
  *
  * @author Julian
  */
-public class ScheduleModel
-{
+public class ScheduleModel {
 
-  private Lesson[][][] timetable = new Lesson[32][5][12];
-  private List<String> kuerzel = new ArrayList<>(90);
-  private List<String> classes = new ArrayList<>(30);
-  private List<String> classrooms = new ArrayList<>(50);
+    private Lesson[][][] timetable = new Lesson[32][5][12];
+    private List<String> kuerzel = new ArrayList<>(90);
+    private List<String> classes = new ArrayList<>(30);
+    private List<String> classrooms = new ArrayList<>(50);
 
-  private static final ScheduleModel instance = new ScheduleModel();
+    private static final ScheduleModel instance = new ScheduleModel();
 
-  private ScheduleModel()
-  {
+    private ScheduleModel() {
 
-  }
-
-  public static ScheduleModel getInstance()
-  {
-    return instance;
-  }
-
-  public Lesson[][] analyzeDoc(Document d, int calweek, int index)
-  {
-    Lesson[][] schedule = new Lesson[5][12];
-    int[] doneCells = new int[12];
-
-    for (int i = 0; i < doneCells.length; i++)
-    {
-      doneCells[i] = 0;
     }
 
-    if (d == null)
-    {
-      Log.log("Not a document");
-      return null;
+    public static ScheduleModel getInstance() {
+        return instance;
     }
 
-    HTMLFontElement f = (HTMLFontElement) d.getElementsByTagName("font").item(1);
-    
-    String klasse = removeLineBreak(f.getTextContent());
+    public Lesson[][] analyzeDoc(Document d, int calweek, int index) {
+        Lesson[][] schedule = new Lesson[5][12];
+        int[] doneCells = new int[12];
 
-    if (!this.classes.contains(klasse))
-    {
-      this.classes.add(klasse);
-    }
+        for (int i = 0; i < doneCells.length; i++) {
+            doneCells[i] = 0;
+        }
 
-    NodeList tables = d.getElementsByTagName("table");
+        if (d == null) {
+            Log.log("Not a document");
+            return null;
+        }
 
-    HTMLTableElement table = (HTMLTableElement) tables.item(0);
+        HTMLFontElement f = (HTMLFontElement) d.getElementsByTagName("font").item(1);
 
-    HTMLCollection rows = table.getRows();
+        String klasse = removeLineBreak(f.getTextContent());
 
-    for (int i = 1; i < rows.getLength(); i = i + 2)
-    {
-      HTMLTableRowElement row = (HTMLTableRowElement) rows.item(i);
+        if (!this.classes.contains(klasse)) {
+            this.classes.add(klasse);
+        }
 
-      HTMLCollection cells = row.getCells();
+        NodeList tables = d.getElementsByTagName("table");
 
-      for (int j = 1; j < cells.getLength(); j++)
-      {
-        if (schedule[j - 1 + doneCells[(i - 1) / 2]][((i - 1) / 2)] == null)
-        {
+        HTMLTableElement table = (HTMLTableElement) tables.item(0);
 
-          LinkedList<String> teachers = new LinkedList<>();
-          String subject = "???";
-          LinkedList<String> classrooms = new LinkedList<>();
-          boolean isLesson = true;
+        HTMLCollection rows = table.getRows();
 
+        for (int i = 1; i < rows.getLength(); i = i + 2) {
+            HTMLTableRowElement row = (HTMLTableRowElement) rows.item(i);
 
-          HTMLTableCellElement cell = (HTMLTableCellElement) cells.item(j);
+            HTMLCollection cells = row.getCells();
 
-          HTMLTableElement inTable = (HTMLTableElement) cell.getChildNodes().item(0);
-          HTMLCollection inRows = inTable.getRows();
+            for (int j = 1; j < cells.getLength(); j++) {
+                if (schedule[j - 1 + doneCells[(i - 1) / 2]][((i - 1) / 2)] == null) {
 
-          for (int k = 0; k < inRows.getLength(); k++)
-          {
-            HTMLTableRowElement inRow = (HTMLTableRowElement) inRows.item(k);
-            HTMLCollection inCells = inRow.getCells();
+                    LinkedList<String> teachers = new LinkedList<>();
+                    String subject = "???";
+                    LinkedList<String> classrooms = new LinkedList<>();
+                    boolean isLesson = true;
 
-            if (k == 0)
-            {
-              if (removeSigns(inCells.item(0).getTextContent().trim()).length() > 5)
-              {
-                // (todo, derweil wird nur gelöscht)
-                isLesson = false;
-              }
-              else
-              {
-                subject = removeSigns(inCells.item(0).getTextContent().trim());
-              }
+                    HTMLTableCellElement cell = (HTMLTableCellElement) cells.item(j);
 
-            }
-            else
-            {
-              if (removeSigns(inCells.item(0).getTextContent().trim()).length() > 3)
-              {
-                // (todo, derweil wird nur gelöscht)
-                isLesson = false;
-              }
-              else
-              {
-                String classroom;
+                    HTMLTableElement inTable = (HTMLTableElement) cell.getChildNodes().item(0);
+                    HTMLCollection inRows = inTable.getRows();
 
-                if (inCells.getLength() == 1)
-                {
-                  classroom = "???";
-                }
-                else
-                {
-                  classroom = removeSigns(inCells.item(1).getTextContent().trim());
-                }
+                    for (int k = 0; k < inRows.getLength(); k++) {
+                        HTMLTableRowElement inRow = (HTMLTableRowElement) inRows.item(k);
+                        HTMLCollection inCells = inRow.getCells();
 
-                String kuerzel = removeSigns(inCells.item(0).getTextContent().trim());
-                teachers.add(kuerzel);
+                        if (k == 0) {
+                            if (removeSigns(inCells.item(0).getTextContent().trim()).length() > 5) {
+                                // (todo, derweil wird nur gelöscht)
+                                isLesson = false;
+                            } else {
+                                subject = removeSigns(inCells.item(0).getTextContent().trim());
+                            }
 
-                if (!this.kuerzel.contains(kuerzel) && !kuerzel.equals("---"))
-                {
-                  this.kuerzel.add(kuerzel);
-                }
+                        } else {
+                            if (removeSigns(inCells.item(0).getTextContent().trim()).length() > 3) {
+                                // (todo, derweil wird nur gelöscht)
+                                isLesson = false;
+                            } else {
+                                String classroom;
 
-                classrooms.add(classroom);
+                                if (inCells.getLength() == 1) {
+                                    classroom = "???";
+                                } else {
+                                    classroom = removeSigns(inCells.item(1).getTextContent().trim());
+                                }
 
-                if (!this.classrooms.contains(classroom) && !classroom.equals("???") && !classroom.equals("---"))
-                {
-                  this.classrooms.add(classroom);
-                }
-              }
-            }
-          }
+                                String kuerzel = removeSigns(inCells.item(0).getTextContent().trim());
+                                teachers.add(kuerzel);
 
-          for (int k = 0; k < (Integer.parseInt(cell.getAttribute("rowspan")) / 2); k++)
-          {
-            int spalte = j - 1 + doneCells[(i - 1) / 2];
-            int zeile = (i - 1) / 2 + k;
+                                if (!this.kuerzel.contains(kuerzel) && !kuerzel.equals("---")) {
+                                    this.kuerzel.add(kuerzel);
+                                }
+
+                                classrooms.add(classroom);
+
+                                if (!this.classrooms.contains(classroom) && !classroom.equals("???") && !classroom.equals("---")) {
+                                    this.classrooms.add(classroom);
+                                }
+                            }
+                        }
+                    }
+
+                    for (int k = 0; k < (Integer.parseInt(cell.getAttribute("rowspan")) / 2); k++) {
+                        int spalte = j - 1 + doneCells[(i - 1) / 2];
+                        int zeile = (i - 1) / 2 + k;
 
             //System.out.println("Spalte: " + spalte + ", Zeile: " + zeile);
+                        if (isLesson && schedule[spalte][zeile] == null) {
+                            Lesson l = new Lesson(teachers.toArray(new String[0]), subject, klasse, classrooms.toArray(new String[0]), calweek);
+                            schedule[spalte][zeile] = l;
 
-            if (isLesson && schedule[spalte][zeile] == null)
-            {
-              Lesson l = new Lesson(teachers.toArray(new String[0]), subject, klasse, classrooms.toArray(new String[0]), calweek);
-              schedule[spalte][zeile] = l;
-
+                        }
+                    }
+                } else {
+                    //System.out.println("longer lesson!");
+                    doneCells[(i - 1) / 2]++;
+                    j--;
+                }
             }
-          }
-        }
-        else
-        {
-          //System.out.println("longer lesson!");
-          doneCells[(i - 1) / 2]++;
-          j--;
-        }
-      }
 
+        }
+
+        return schedule;
     }
 
-    return schedule;
-  }
+    private String[] convertClassrooms(List<String> classrooms) {
+        String[] array = new String[classrooms.size()];
 
-  private String[] convertClassrooms(List<String> classrooms)
-  {
-    String[] array = new String[classrooms.size()];
-
-    for (int i = 0; i < classrooms.size(); i++)
-    {
-      array[i] = classrooms.get(i);
-    }
-
-    return array;
-  }
-
-  public Lesson[][] getTeacherLessons(String ku)
-  {
-    Log.log("Retrieve lessons of selected teacher...");
-      Lesson[][] table = new Lesson[5][12];
-
-    for (int i = 0; i < timetable.length; i++)
-    {
-      for (int j = 0; j < timetable[i].length; j++)
-      {
-        for (int k = 0; k < timetable[i][j].length; k++)
-        {
-          Lesson l = timetable[i][j][k];
-
-          if (l != null && contains(l.getTeachers(), ku))
-          {
-            table[j][k] = l;
-          }
+        for (int i = 0; i < classrooms.size(); i++) {
+            array[i] = classrooms.get(i);
         }
-      }
+
+        return array;
     }
-    
-    return table;
-  }
 
-  public Lesson[][] getClassLessons(String cl)
-  {
-    Log.log("Retrieve lessons of selected class...");
-      Lesson[][] table = new Lesson[5][12];
+    public Lesson[][] getTeacherLessons(String ku) {
+        Log.log("Retrieve lessons of selected teacher...");
+        Lesson[][] table = new Lesson[5][12];
 
-    boolean classfound = false;
-    int i = 0;
-    while (!classfound && i < timetable.length)
-    {
-      int j = 0;
-      int k = 0;
-      boolean found = false;
-      while (!found && j < timetable[0].length)
-      {
-        while (!found && k < timetable[0][0].length)
-        {
-          if (timetable[i][j][k] != null)
-          {
-            found = true;
+        for (int i = 0; i < timetable.length; i++) {
+            for (int j = 0; j < timetable[i].length; j++) {
+                for (int k = 0; k < timetable[i][j].length; k++) {
+                    Lesson l = timetable[i][j][k];
 
-            if (timetable[i][j][k].getKlasse().equals(cl))
-            {
-              table = timetable[i];
-              classfound = true;
+                    if (l != null && contains(l.getTeachers(), ku)) {
+                        table[j][k] = l;
+                    }
+                }
             }
-          }
-
-          k++;
         }
-        j++;
-      }
-      i++;
+
+        return table;
     }
-    
-    return table;
-  }
 
-  public Lesson[][] getClassroomLessons(String cl)
-  {
-    Log.log("Retrieve lessons of selected room...");
-      Lesson[][] table = new Lesson[5][12];
+    public Lesson[][] getClassLessons(String cl) {
+        Log.log("Retrieve lessons of selected class...");
+        Lesson[][] table = new Lesson[5][12];
 
-    for (int i = 0; i < timetable.length; i++)
-    {
-      for (int j = 0; j < timetable[i].length; j++)
-      {
-        for (int k = 0; k < timetable[i][j].length; k++)
-        {
-          Lesson l = timetable[i][j][k];
+        boolean classfound = false;
+        int i = 0;
+        while (!classfound && i < timetable.length) {
+            int j = 0;
+            int k = 0;
+            boolean found = false;
+            while (!found && j < timetable[0].length) {
+                while (!found && k < timetable[0][0].length) {
+                    if (timetable[i][j][k] != null) {
+                        found = true;
 
-          if (l != null && contains(l.getClassrooms(), cl))
-          {
-            table[j][k] = l;
-          }
+                        if (timetable[i][j][k].getKlasse().equals(cl)) {
+                            table = timetable[i];
+                            classfound = true;
+                        }
+                    }
+
+                    k++;
+                }
+                j++;
+            }
+            i++;
         }
-      }
+
+        return table;
     }
-    return table;
-  }
 
-  public void loadAllLessons(WebEngine we, int week)
-  {
-    Log.log("Start loading all lessons...");
-      classes.clear();
-    kuerzel.clear();
-    classrooms.clear();
+    public Lesson[][] getClassroomLessons(String cl) {
+        Log.log("Retrieve lessons of selected room...");
+        Lesson[][] table = new Lesson[5][12];
 
-    we.getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>()
-    {
+        for (int i = 0; i < timetable.length; i++) {
+            for (int j = 0; j < timetable[i].length; j++) {
+                for (int k = 0; k < timetable[i][j].length; k++) {
+                    Lesson l = timetable[i][j][k];
 
-      int counter = 0   ;
-
-      @Override
-      public void changed(ObservableValue<? extends State> observable, State oldValue, State newValue)
-      {
-
-        if (newValue == State.SUCCEEDED)
-        {
-            System.out.println(counter);
-          
-
-          if (counter < timetable.length)
-          {
-              timetable[counter] = analyzeDoc(we.getDocument(), week, counter);
-            counter++;
-            String url = "https://supplierplan.htl-kaindorf.at/supp_neu/" + (week) + "/c/c" + String.format("%05d", counter) + ".htm";
-            we.load(url);
-              System.out.println(url);
-            Log.log("Browsing to "+url);
-          }
-          else
-          {
-            //WebBrowserTest.printAllLessons(timetable);
-            we.getLoadWorker().stateProperty().removeListener(this);
-            Log.log("Finished loading");
-          }
+                    if (l != null && contains(l.getClassrooms(), cl)) {
+                        table[j][k] = l;
+                    }
+                }
+            }
         }
-      }
-    });
-
-    we.load("https://supplierplan.htl-kaindorf.at/supp_neu/" + (week) + "/c/c" + String.format("%05d", 1) + ".htm");
-  }
-
-  private boolean contains(String[] a, String k)
-  {
-    boolean b = false;
-
-    for (int i = 0; i < a.length; i++)
-    {
-      if (a[i].equals(k))
-      {
-        b = true;
-      }
+        return table;
     }
 
-    return b;
-  }
+    public void loadAllLessons(WebEngine we, int week) {
+        Log.log("Start loading all lessons...");
+        classes.clear();
+        kuerzel.clear();
+        classrooms.clear();
 
-  private String removeSigns(String s)
-  {
-    char[] ignorableChars
-            =
-            {
-              '.', ',', '\'', '\"', '!', '?', '§', '$',
-              '%', '&', '/', '(', ')', '=', '\\', ']',
-              '[', '{', '}', '#', '+', '*', '~', '\n'
-            };
+        we.getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
 
-    for (char ignorableChar : ignorableChars)
-    {
-      s = s.replace(ignorableChar + "", "");
+            int counter = 0;
+
+            @Override
+            public void changed(ObservableValue<? extends State> observable, State oldValue, State newValue) {
+
+                if (newValue == State.SUCCEEDED) {
+                    if (counter < timetable.length) {
+                        timetable[counter] = analyzeDoc(we.getDocument(), week, counter);
+                        counter++;
+                        String url = "https://supplierplan.htl-kaindorf.at/supp_neu/" + (week) + "/c/c" + String.format("%05d", counter) + ".htm";
+                        we.load(url);
+                        System.out.println(url);
+                        Log.log("Browsing to " + url);
+                    } else {
+                        //WebBrowserTest.printAllLessons(timetable);
+                        we.getLoadWorker().stateProperty().removeListener(this);
+                        Log.log("Finished loading");
+                    }
+                }
+            }
+        });
+
+        we.load("https://supplierplan.htl-kaindorf.at/supp_neu/" + (week) + "/c/c" + String.format("%05d", 1) + ".htm");
     }
 
-    return s;
-  }
+    private boolean contains(String[] a, String k) {
+        boolean b = false;
 
-  public String removeLineBreak(String s)
-  {
-    String s1 = "";
+        for (int i = 0; i < a.length; i++) {
+            if (a[i].equals(k)) {
+                b = true;
+            }
+        }
 
-    s1 = s.replace("\n", "");
+        return b;
+    }
 
-    return s1;
-  }
+    private String removeSigns(String s) {
+        char[] ignorableChars
+                = {
+                    '.', ',', '\'', '\"', '!', '?', '§', '$',
+                    '%', '&', '/', '(', ')', '=', '\\', ']',
+                    '[', '{', '}', '#', '+', '*', '~', '\n'
+                };
 
-  public List<String> getClasses()
-  {
-    return classes;
-  }
+        for (char ignorableChar : ignorableChars) {
+            s = s.replace(ignorableChar + "", "");
+        }
 
-  public List<String> getKuerzel()
-  {
-    return kuerzel;
-  }
+        return s;
+    }
 
-  public Lesson[][][] getTimetable()
-  {
-    return timetable;
-  }
+    public String removeLineBreak(String s) {
+        String s1 = "";
 
-  public List<String> getClassrooms()
-  {
-    return classrooms;
-  }
+        s1 = s.replace("\n", "");
+
+        return s1;
+    }
+
+    public List<String> getClasses() {
+        return classes;
+    }
+
+    public List<String> getKuerzel() {
+        return kuerzel;
+    }
+
+    public Lesson[][][] getTimetable() {
+        return timetable;
+    }
+
+    public List<String> getClassrooms() {
+        return classrooms;
+    }
 
 }
